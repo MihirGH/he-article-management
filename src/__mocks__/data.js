a1756ff6aef9b9f2cb4e30245e1758e0650e71b6
MihirGH <hashmihir@gmail.com>
2024-09-11 00:22:31 +0530

    feat: add sample and main tests

---
+++ src/__mocks__/data.js
@@ -0,0 +1,22 @@
+export const TEST_ARTICLES = [
+  {
+    id: 1,
+    title: 'JS and React',
+    tags: ['React', 'JavaScript'],
+  },
+  {
+    id: 2,
+    title: 'CSS and JS',
+    tags: ['JavaScript', 'CSS'],
+  },
+  {
+    id: 3,
+    title: 'CSS and React',
+    tags: ['React', 'CSS'],
+  },
+  {
+    id: 4,
+    title: 'HTML',
+    tags: ['HTML'],
+  },
+];
