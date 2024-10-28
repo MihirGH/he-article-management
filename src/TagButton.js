4b0a42d7f3dea55083b5b238729ea9999960f03a
MihirGH <hashmihir@gmail.com>
2024-10-28 18:06:27 +0530

    chore: add disabled state styles

--- src/TagButton.js
+++ src/TagButton.js
@@ -6,6 +6,8 @@ export const TagButton = ({ children, disabled, onClick, isSelected }) => (
     disabled={disabled}
     className={cn('border px-2 py-0.5 rounded hover:border-blue-400', {
       'border-blue-400': isSelected,
+      'border-gray-400 hover:border-gray-400 opacity-60 cursor-not-allowed':
+        disabled,
     })}
     onClick={onClick}
   >
